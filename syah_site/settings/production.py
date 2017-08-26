0fc5e50e78ce7ae79c36fd6a20c99b21d4f62f82
Tom Heeney <tom.heeney@gmail.com>
2017-08-26 16:43:54 +0100

    Configured RDS database

--- syah_site/settings/production.py
+++ syah_site/settings/production.py
@@ -6,3 +6,15 @@ SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 CSRF_COOKIE_SECURE = True
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
