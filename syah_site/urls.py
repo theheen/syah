910b1eda90cfd7ae51514cf75561d10acf2b922c
Tom Heeney <tom.heeney@gmail.com>
2017-08-27 14:24:51 +0100

    Removed debug toolbar from urls to commit wit DEBUG=True

--- syah_site/urls.py
+++ syah_site/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     url(r'^$', HomepageView.as_view(), name='homepage'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
-            ] + urlpatterns
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#             url(r'^__debug__/', include(debug_toolbar.urls)),
+#             ] + urlpatterns
