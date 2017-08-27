2e7c8c4163f33dc1136af9077bb950f528a72723
Tom Heeney <tom.heeney@gmail.com>
2017-08-27 16:14:33 +0100

    Added success message for superuser creation

--- releases/management/commands/createsu.py
+++ releases/management/commands/createsu.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 
 from syah_site.settings.base import get_env_variable
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="heendog").exists():
             pw = get_env_variable("DJANGO_ADMIN_PASSWORD")
             User.objects.create_superuser("heendog", "admin@admin.com", pw)
+            self.stdout.write(
+                self.style.SUCCESS('Successfully created new super user'))
