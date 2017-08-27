e287f88db5d33c673a76618fc06211facfd9d220
Tom Heeney <tom.heeney@gmail.com>
2017-08-27 14:47:40 +0100

    Added createsu

---
+++ releases/management/commands/createsu.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from syah_site.settings.base import get_env_variable
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="heendog").exists():
+            pw = get_env_variable("DJANGO_ADMIN_PASSWORD")
+            User.objects.create_superuser("heendog", "admin@admin.com", pw)
