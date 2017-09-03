c47a251cd98dd46b1fca8a77a3296e87e0a757fc
Tom Heeney <tom.heeney@gmail.com>
2017-09-03 17:21:16 +0100

    Added is_published to blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 from .models import Blog
+
 # Register your models here.
 
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'is_published', 'pub_datetime')
 
 
 admin.site.register(Blog, BlogAdmin)
