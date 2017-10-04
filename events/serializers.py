36e35601fc4c448de0a3612bcf6ffefe71710775
Tom Heeney <tom.heeney@gmail.com>
2017-10-04 11:42:41 +0100

    Added public API for events.

---
+++ events/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Event, Venue
+
+
+class VenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venue
+        fields = ('name', 'location', 'url')
+
+
+class EventSerializer(serializers.ModelSerializer):
+    venue = VenueSerializer()
+
+    class Meta:
+        model = Event
+        fields = ('venue', 'date', 'ticket_url', 'info')
--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView
+from rest_framework.generics import ListAPIView
 
 import datetime
 
 from .models import Event
+from .serializers import EventSerializer
 
 
 # Create your views here.
@@ -16,3 +18,10 @@ class EventListView(ListView):
         context = super(EventListView, self).get_context_data(**kwargs)
         context['events_active'] = True
         return context
+
+
+class EventAPIListView(ListAPIView):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        return Event.objects.filter(date__gte=datetime.date.today())
