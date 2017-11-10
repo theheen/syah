5d5687860d35bb4b314cfc7677aa7bebb887ef21
Tom Heeney <tom.heeney@gmail.com>
2017-11-10 13:11:13 +0000

    Set up Travis

---
+++ syah_site/settings/ci.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+    }
+}
